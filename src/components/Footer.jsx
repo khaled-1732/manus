638445c102d7f24f36c6f8ad3ea5c7a8a20833be
khaled-1732 <volsca.khaled@gmail.com>
2025-06-09 21:16:54 +0100

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer className="bg-darkslate text-white p-4 text-center">
+    <p>&copy; {new Date().getFullYear()} The Gentleman's Cut. All rights reserved.</p>
+  </footer>
+);
+
+export default Footer;
